a1d7489ea8679243d37563a48daf1f81469e1670
tushargola1 <87809407+tushargola1@users.noreply.github.com>
2022-08-29 01:40:14 +0530

    Add files via upload

---
+++ multipage/js/course.js
@@ -0,0 +1,22 @@
+function ugProgram(){
+    window.open("./course_UG.html" , "_self")
+}
+function pgProgram(){
+    window.open("./pg.html" , "_self")
+}
+function medical(){
+    window.open("./mdeical.html" , "_self")
+}
+function humanity(){
+    window.open("./humanity.html" , "_self")
+}
+function visualmedia(){
+    window.open("./Visual Media.html" , "_self")
+}
+function adventure(){
+    window.open("./adventure.html" , "_self")
+}
+function submitModal(){
+    window,open("./contact.html")
+    
+}
